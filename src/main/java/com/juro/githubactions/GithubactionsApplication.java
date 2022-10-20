d9daa8c2f490488d2f4a8eba83b0e41fbabf00d5
Kim Jingun <22jg.kim@juroinstruments.com>
2022-10-20 16:55:23 +0900

    commit

--- src/main/java/com/juro/githubactions/GithubactionsApplication.java
+++ src/main/java/com/juro/githubactions/GithubactionsApplication.java
@@ -2,13 +2,22 @@ package com.juro.githubactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class GithubactionsApplication {
 
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(GithubactionsApplication.class, args);
-		System.out.println("test");
 	}
 
 }
