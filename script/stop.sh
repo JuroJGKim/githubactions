7fd4368c793aaf953293a3b4cabb5dfb522a78be
Kim Jingun <22jg.kim@juroinstruments.com>
2022-10-21 13:04:24 +0900

    commit

--- script/stop.sh
+++ script/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/jurogithubactions/jurogithubactions.jar"
+JAR_FILE="$PROJECT_ROOT/jurogithubactions.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
