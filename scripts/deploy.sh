d7ffe3cdaff589383d0bf11baa712f5f16ab44bc
Kim Jingun <22jg.kim@juroinstruments.com>
2022-10-24 13:14:50 +0900

    deploy.sh commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-echo "start" >> ~/deployscript.log
 REPOSITORY=/home/ubuntu/app/step2
 PROJECT_NAME=githubactions #해당 위치에 properties에 작성한 프로젝트명과 동일하게 작성합니다.
 
-echo "Build 파일 복사" >> ~/deployscript.log
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "현재 구동 중인 애플리케이션 pid 확인" >> ~/deployscript.log
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f $PROJECT_NAME)
 
-echo "현재 구동 중인 애플리케이션" >> ~/deployscript.log
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
